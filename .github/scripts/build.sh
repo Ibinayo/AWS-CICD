44a6ddda4644404b661158d672b0d2c59cca2c44
Ibinayo <113690479+Ibinayo@users.noreply.github.com>
2025-02-08 23:36:10 -0500

    Update build.sh

--- .github/scripts/build.sh
+++ .github/scripts/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -xe  # Stop on error, print commands
+
 
 # Change to the correct project directory before running Maven
 cd "$(dirname "$0")/../../spring-boot-hello-world-example" || exit 1
