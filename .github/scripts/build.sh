cb3594570e76ea2196b863010ec75db89f48a107
Ibinayo <113690479+Ibinayo@users.noreply.github.com>
2025-02-08 23:17:06 -0500

    Update build.sh

--- .github/scripts/build.sh
+++ .github/scripts/build.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-set -xe
- 
-  # Maven is used to build  and create a war file.
-  mvn -Dmaven.test.skip=true clean install
+set -xe  # Stop on error, print commands
+
+# Change to the correct project directory before running Maven
+cd "$(dirname "$0")/../../spring-boot-hello-world-example" || exit 1
+
+# Run Maven build
+mvn -Dmaven.test.skip=true clean install
